433dedd3c6d8f632bf86064a70468cef731b1e9d
sangamSanjeet <factsbe0@gmail.com>
2024-08-18 20:58:21 +0530

    updated no page found

--- src/pages/NoMatch.tsx
+++ src/pages/NoMatch.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 const NoMatch = () => {
-	return <div>NoMatch</div>;
+	return (
+		<div className={"h-screen flex items-center justify-center flex-col"}>
+			<h1>Invalid Page Url</h1>
+			<Link
+				to={"/"}
+				className="p-3 border rounded-lg mt-5 hover:bg-slate-100 hover:text-black duration-200 hover:scale-110"
+			>
+				Back to Home
+			</Link>
+		</div>
+	);
 };
 
 export default NoMatch;
